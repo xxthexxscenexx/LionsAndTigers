9161dbfcdb1163678b9d7e4787cc1a51b9e5f3c6
Rosemarie Day <whyarntrobotsincontrolofearth@gmail.com>
2015-05-29 18:42:46 -0400

    Added tiger struct in a swift file and used this to update tiger struct properties in swift which then were used in the UI to display properties and image

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,20 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Rosemarie Day on 5/29/15.
+//  Copyright (c) 2015 rjd. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    
+    // properties for the tiger struct which include age, name, breed and image used for background
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"") // takes a parameter for the image name
+    
+}
--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -20,6 +20,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // sets up the Tiger struct
+        var myTiger = Tiger()
+        myTiger.name = "Tigger"
+        myTiger.age = 3
+        myTiger.breed = "Bengal"
+        myTiger.image = UIImage(named: "BengalTiger.jpg")
+        
+        // uses the values of the Tiger struct to change the UI 
+        imageView.image = myTiger.image
+        nameLabel.text = myTiger.name
+        ageLabel.text = "\(myTiger.age)"
+        breedLabel.text = myTiger.breed
+        
     }
 
     override func didReceiveMemoryWarning() {
